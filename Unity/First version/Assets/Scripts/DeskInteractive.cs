0c6f75551433cde308de7676188d661e45ed1beb
chuyenying <410835046@gms.ndhu.edu.tw>
2021-10-10 16:43:36 +0800

    把projet弄進來

---
+++ Unity/First version/Assets/Scripts/DeskInteractive.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DeskInteractive : MonoBehaviour
+{
+    public GameObject people,focus_table,butt,firstperson_object;  //people為跟隨人的攝影機,focus_table拍桌面的攝影機,butt為button
+    public Text butt_t,t;   //butt_t為button的文字, t為button旁邊的文字
+    private bool E_use = false;     //紀錄是否按下了E鍵，如果按下了E鍵才可以按F退出桌面
+    private Vector3 player_pos;    //用來固定角色的位置，防止角色在聚焦桌子後亂跑，離開桌子後卻發現自己根本不在桌子旁邊
+    public GameObject[] ObjectOnDesk = new GameObject [8];  //此陣列存放存放桌子上的物件
+    private int index=0;
+    private Color OriginColor;
+    void Start()
+    {
+        focus_table.SetActive(false);
+        butt_and_text_close();
+    }
+    void Update()
+    {
+        if (E_use == true)
+        {
+            firstperson_object.gameObject.transform.position = player_pos;
+            t.text = "離開桌面";
+            butt_t.text = "F";
+            butt_and_text_open();
+
+            OriginColor = ObjectOnDesk[0].GetComponent<Renderer>().material.color;
+            
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                
+                Debug.Log(index);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+               
+                Debug.Log(index);
+            }
+            if (Input.GetKey(KeyCode.F))
+            {
+                focus_table.SetActive(false);
+                people.SetActive(true);
+                Cursor.lockState = CursorLockMode.Locked;
+                E_use = false;
+                butt_and_text_close();
+            }   
+        }
+    }
+    void OnCollisionStay(Collision other) 
+    {
+        if (other.gameObject.CompareTag("Player"))
+         {
+            t.text = "查看桌面";
+            butt_t.text = "E";
+            butt_and_text_open();
+
+            if (Input.GetKey(KeyCode.E) && E_use==false)    //沒按過E鍵才可以開啟桌面
+            {
+                player_pos = firstperson_object.gameObject.transform.position;
+                butt_and_text_close();
+                focus_table.SetActive(true);
+                people.SetActive(false);
+                Cursor.lockState = CursorLockMode.Confined;
+                E_use = true;
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        butt_and_text_close();
+    }
+
+    void butt_and_text_open()
+    {
+        butt.gameObject.SetActive(true);
+        butt_t.gameObject.SetActive(true);
+        t.gameObject.SetActive(true);
+
+    }
+    void butt_and_text_close()
+    {
+        butt.gameObject.SetActive(false);
+        butt_t.gameObject.SetActive(false);
+        t.gameObject.SetActive(false);
+    }
+}
---
+++ Unity/First version/Assets/Scripts/controlpeople.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlpeople : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 2f;    //角色移動速度
+
+    void Update()
+    {
+        speed = 2;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 5;    //按shift可加速
+        }
+
+        //利用Input.GetAxis("Horizontal")、("Vertical")
+        //來偵測WASD的值，值介於-1~1;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
