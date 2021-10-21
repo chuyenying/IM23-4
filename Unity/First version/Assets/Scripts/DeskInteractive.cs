e100dcf8ba974e8244753e4666fe1df6505b7c1e
chuyenying <410835046@gms.ndhu.edu.tw>
2021-10-21 23:45:06 +0800

    可選鉛筆盒
    1. 可選物件 LeftArrow,  RightArrow
    2. 可點開鉛筆盒 Enter
    3. 可跳出鉛筆盒 BackSpace

--- Unity/First version/Assets/Scripts/DeskInteractive.cs
+++ Unity/First version/Assets/Scripts/DeskInteractive.cs
@@ -4,38 +4,47 @@ using UnityEngine;
 using UnityEngine.UI;
 public class DeskInteractive : MonoBehaviour
 {
-    public GameObject people,focus_table,butt,firstperson_object;  //people為跟隨人的攝影機,focus_table拍桌面的攝影機,butt為button
-    public Text butt_t,t;   //butt_t為button的文字, t為button旁邊的文字
-    private bool E_use = false;     //紀錄是否按下了E鍵，如果按下了E鍵才可以按F退出桌面
+    public GameObject focus_people,focus_table,focus_pencilbox,butt,firstperson_object,butt2;  //focus_people為跟隨人的攝影機,focus_table拍桌面的攝影機,butt為button
+    public Text butt_t,t,butt_t2,t2;   //butt_t為button的文字, t為button旁邊的文字
+    private bool E_use = false,LookAtPB=false;     //紀錄是否按下了E鍵，如果按下了E鍵才可以按F退出桌面。LookAtPB紀錄是否正在看鉛筆盒
     private Vector3 player_pos;    //用來固定角色的位置，防止角色在聚焦桌子後亂跑，離開桌子後卻發現自己根本不在桌子旁邊
     public GameObject[] ObjectOnDesk = new GameObject [8];  //此陣列存放存放桌子上的物件
-    private int index=0;
-    private Color[] ObjectOnDesk_Color= new Color[8];
+    private int index=0;        //ObjectOnDesk的索引值
+    private Color[] ObjectOnDesk_Color= new Color[8];   //儲存物件原本的顏色
     void Start()
     {
-        focus_table.SetActive(false);
-        butt_and_text_close();
-        for(int i = 0; i < ObjectOnDesk.Length; i++)
+        focus_table.SetActive(false);   //一開始桌子跟鉛筆盒的相機都先關閉
+        focus_pencilbox.SetActive(false);
+        butt_and_text_close();          //上下button也全關閉
+        butt_and_text_close2();
+
+        for (int i = 0; i < ObjectOnDesk.Length; i++)
         {
-            ObjectOnDesk_Color[i] = ObjectOnDesk[i].GetComponent<Renderer>().material.color;
+            ObjectOnDesk_Color[i] = ObjectOnDesk[i].GetComponent<Renderer>().material.color;        //先把物件原先的顏色儲存起來
         }
     }
     void Update()
     {
         if (E_use == true)
         {
-            firstperson_object.gameObject.transform.position = player_pos;
-            t.text = "離開桌面";
+            firstperson_object.gameObject.transform.position = player_pos;  //把剛剛存起來的位置給角色，讓角色無法移動
             butt_t.text = "F";
-            butt_and_text_open();
+            if (LookAtPB == false)
+            {
+                t.text = "離開桌面";
+                butt_and_text_open();
+            }
 
-            if (Input.GetKeyDown(KeyCode.RightArrow))
+            Choose_Table();
+
+            if (Input.GetKeyDown(KeyCode.RightArrow) && LookAtPB==false)    //查看鉛筆盒的時候就不能選物件
             {
                 
-                if (index == ObjectOnDesk.Length-1)
+                if (index == ObjectOnDesk.Length-1) //已經選到最右邊的物件仍然按右鍵
                 {
-                    ObjectOnDesk[0].GetComponent<Renderer>().material.color = Color.yellow;
-                    ObjectOnDesk[index].GetComponent<Renderer>().material.color = ObjectOnDesk_Color[index];
+                    ObjectOnDesk[0].GetComponent<Renderer>().material.color = Color.yellow; //則把最左邊的物體變成黃色(看起來像選最左邊的物件)
+                    ObjectOnDesk[index].GetComponent<Renderer>().material.color = ObjectOnDesk_Color[index];    
+                    //把上一個物件變回原來的顏色,ObjectOnDesk_Color[]用來儲存物件原本的顏色
                     index = 0;
                 }
                 else
@@ -46,7 +55,7 @@ public class DeskInteractive : MonoBehaviour
                 }
              }
 
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && LookAtPB == false)   //正在看鉛筆盒就不能選物件
             {
 
                 if (index == 0)
@@ -63,13 +72,18 @@ public class DeskInteractive : MonoBehaviour
                 }
             }
 
-            if (Input.GetKey(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.F) && LookAtPB==false )    //沒有查看鉛筆盒的情況按F才可以離開桌面
             {
-                focus_table.SetActive(false);
-                people.SetActive(true);
-                Cursor.lockState = CursorLockMode.Locked;
+                index = 0;
+                firstperson_object.gameObject.SetActive(true);
+                Table_SwitchTo_People();
                 E_use = false;
                 butt_and_text_close();
+                butt_and_text_close2();
+                for (int i = 0; i < ObjectOnDesk.Length; i++)
+                {
+                    ObjectOnDesk[i].GetComponent<Renderer>().material.color = ObjectOnDesk_Color[i];
+                }
             }   
         }
     }
@@ -83,13 +97,12 @@ public class DeskInteractive : MonoBehaviour
 
             if (Input.GetKey(KeyCode.E) && E_use==false)    //沒按過E鍵才可以開啟桌面
             {
-                player_pos = firstperson_object.gameObject.transform.position;
-                butt_and_text_close();
-                focus_table.SetActive(true);
-                people.SetActive(false);
-                Cursor.lockState = CursorLockMode.Confined;
+                firstperson_object.gameObject.SetActive(false); //打開桌面後，角色就暫時看不到
+                player_pos = firstperson_object.gameObject.transform.position;  //把角色按下E當下的位置記錄起來。
+                butt_and_text_close();                         //把BUTTON跟文字關閉
+                People_SwitchTo_Table();                       
                 E_use = true;
-                ObjectOnDesk[0].GetComponent<Renderer>().material.color = Color.yellow;
+                ObjectOnDesk[0].GetComponent<Renderer>().material.color = Color.yellow; //剛進到桌面的時候會自動先選鉛筆盒。
             }
         }
     }
@@ -99,17 +112,87 @@ public class DeskInteractive : MonoBehaviour
         butt_and_text_close();
     }
 
+    // **** 以下是兩個button及個別旁邊的文字的開啟關閉 ****
     void butt_and_text_open()
     {
         butt.gameObject.SetActive(true);
-        butt_t.gameObject.SetActive(true);
         t.gameObject.SetActive(true);
-
     }
     void butt_and_text_close()
     {
         butt.gameObject.SetActive(false);
-        butt_t.gameObject.SetActive(false);
         t.gameObject.SetActive(false);
     }
+    void butt_and_text_open2()
+    {
+        butt2.gameObject.SetActive(true);
+        t2.gameObject.SetActive(true);
+
+    }
+    void butt_and_text_close2()
+    {
+        butt2.gameObject.SetActive(false);
+        t2.gameObject.SetActive(false);
+    }
+
+    // ----------------------------------------------------
+
+    // **** 以下是相機的切換 ****
+    void People_SwitchTo_Table()    //將跟隨人的相機關閉，並打開聚焦桌面的相機(用於查看桌面)
+    {
+        focus_people.SetActive(false);
+        focus_table.SetActive(true);
+    }
+
+    void Table_SwitchTo_People()    //將聚焦桌子的相機關閉，打開聚焦人的(用於離開桌面)
+    {
+        focus_people.SetActive(true);
+        focus_table.SetActive(false);
+    }
+
+    void Table_SwitchTo_PB()        //將聚焦桌面的相機關閉，打開聚焦鉛筆盒(用於從桌面按Enter選鉛筆盒)
+    {
+        focus_pencilbox.SetActive(true);
+        focus_table.SetActive(false);
+    }
+    void PB_SwitchTo_Table()        //(用於離開鉛筆盒)
+    {
+        focus_pencilbox.SetActive(false);
+        focus_table.SetActive(true);
+    }
+
+    //--------------------------------------------------------
+
+    // **** 與選擇物件相關****
+    void Choose_Table()     //跟鉛筆盒有關的
+    {
+        if (index == 0)
+        {
+            butt_and_text_open2();
+
+            if (Input.GetKey(KeyCode.Return))
+            {
+                LookAtPB = true;
+                ObjectOnDesk[0].GetComponent<Renderer>().material.color = ObjectOnDesk_Color[0];
+                Table_SwitchTo_PB();
+                butt_and_text_close();
+                butt_t2.text = "Back\nSpace";
+                t2.text = "離開鉛筆盒";
+            }
+            if (Input.GetKeyDown(KeyCode.Backspace))
+            {
+                ObjectOnDesk[0].GetComponent<Renderer>().material.color = Color.yellow;
+                PB_SwitchTo_Table();
+                butt_t2.text = "Enter\n↵";
+                t2.text = "查看鉛筆盒";
+                LookAtPB = false;
+            }
+        }
+        else
+        {
+            PB_SwitchTo_Table();
+            butt_and_text_close2();
+        }
+    }
+    // ----------------------------------------------------------------
 }
