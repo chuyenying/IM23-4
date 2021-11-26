be3a0e159674b49a8b2e84191ea0b4785851a11b
Opal0811 <410835022@gms.ndhu.edu.tw>
2021-11-26 20:21:40 +0800

    動畫走/跑 前後左右
    移動中按下左shift動畫從跑變走
    前後左右對應動畫

--- Unity/First version/Assets/Scripts/ChaMove.cs
+++ Unity/First version/Assets/Scripts/ChaMove.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class ChaMove : MonoBehaviour
 {
-    public float speed = 0.3f;
+    public float speed = 0;
+    public float direction;
     Animator anim;
     Vector3 move;
     float forwardAmount;
@@ -16,7 +17,37 @@ public class ChaMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        speed = 2;
+
+        bool moveforward =Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.UpArrow);
+        bool moveright = Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.RightArrow);
+        bool moveleft = Input.GetKey(KeyCode.A)|| Input.GetKey(KeyCode.LeftArrow);
+        bool movebackward = Input.GetKey(KeyCode.S)|| Input.GetKey(KeyCode.DownArrow);
+        speed = 0;
+        if(moveforward)
+        {
+            direction = 1;
+            speed = 2;
+        }
+        else if(movebackward)
+        {
+            direction = 2;
+            speed = 2;
+        }
+        else if (moveleft)
+        {
+            direction = 3;
+            speed = 2;
+        }
+        else if(moveright)
+        {
+            direction = 4;
+            speed = 2;
+        }
+        else
+        {
+            direction = 1;
+            speed = 0;
+        }
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = 5;    //«öshift¥i¥[³t
@@ -25,12 +56,11 @@ public class ChaMove : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         move = (transform.right * x) + transform.forward * z;
 
-        Vector3 localMove = transform.InverseTransformVector(move);
-        forwardAmount = localMove.z;
         UpdateAnim();
     }
     void UpdateAnim()
     {
-        anim.SetFloat("speed", forwardAmount);
+        anim.SetFloat("speed", speed);
+        anim.SetFloat("Blend", direction);
     }
 }
