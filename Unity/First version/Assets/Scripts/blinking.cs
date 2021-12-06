372a01919e690d7af40f9c3d4e2c671d26c81eee
chuyenying <410835046@gms.ndhu.edu.tw>
2021-12-06 16:58:04 +0800

    眨眼
    可以眨眼但還沒模糊!

---
+++ Unity/First version/Assets/Scripts/blinking.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blinking : MonoBehaviour
+{
+    public Animation topeye, bottomeye;
+    void Start()
+    {
+        StartCoroutine(blink());
+    }
+
+    IEnumerator blink()
+    {
+        yield return new WaitForSeconds(4);
+        topeye.Play();
+        bottomeye.Play();
+    }
+}
