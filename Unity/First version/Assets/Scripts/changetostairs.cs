4ab5ef6ba50b91a4cd06708d04a2b8cd1cf7b92a
chuyenying <410835046@gms.ndhu.edu.tw>
2021-12-08 11:33:33 +0800

    樓梯換場 12/8 11:33
    樓梯換場

---
+++ Unity/First version/Assets/Scripts/changetostairs.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class changetostairs : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene("¨«´Y");
+    }
+}
