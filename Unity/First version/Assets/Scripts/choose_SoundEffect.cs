e2c35f2fe01a899d5528ef955e205f562605b589
chuyenying <410835046@gms.ndhu.edu.tw>
2022-02-05 15:33:52 +0800

    音效修正

--- Unity/First version/Assets/Scripts/choose_SoundEffect.cs
+++ Unity/First version/Assets/Scripts/choose_SoundEffect.cs
@@ -8,7 +8,7 @@ public class choose_SoundEffect : MonoBehaviour
     [SerializeField] private bool do_once=true;  //避免音效重複播放
     void Update()
     {
-        if(Ob.transform.position== new Vector3(-0.484f, 1.35f, -0.853f)) {   //物件飛起來的時候(選物件)
+        if(Ob.transform.position== new Vector3(-0.473f, 3.972f, 23.622f)) {   //物件飛起來的時候(選物件)
             if (do_once) { choose_sound.Play(); } 
             do_once = false;
         } 
