9a8a246ef1d38ce50f42e2ae62a3cd5dffe3fb6f
chuyenying <410835046@gms.ndhu.edu.tw>
2022-03-17 20:49:35 +0800

    0317
    瘋狂做手機

--- Unity/First version/Assets/章節/暫時Scene-用於製作手機UI/手機scripts/Cellphone_UI.cs
+++ Unity/First version/Assets/章節/暫時Scene-用於製作手機UI/手機scripts/Cellphone_UI.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Cellphone_UI : MonoBehaviour
 {
+    [SerializeField] private GameObject MainScreen,Lock_Screen;
+    [SerializeField] private GameObject Remove_Button;
     [SerializeField] private InputField input;
     [SerializeField] private Animator password_wrong_anim;
     public void pressNum(string num)
@@ -12,17 +14,30 @@ public class Cellphone_UI : MonoBehaviour
     }
     public void EraseNum()
     {
-        input.text = input.text.Remove(input.text.Length-1);
+            input.text = input.text.Remove(input.text.Length-1);
     }
     public void checkPassword()
     {
         if (input.text == "0714")
         {
             password_wrong_anim.SetTrigger("open_Lock");
+            StartCoroutine(wait());
         }
         else
         {
             password_wrong_anim.SetTrigger("wrong_password");
         }
+    }
+    public void Update()
+    {
+        if (input.text.Length != 0){Remove_Button.SetActive(true);}
+        else{Remove_Button.SetActive(false);}
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Lock_Screen.SetActive(false);
+        MainScreen.SetActive(true);
     }
 }
