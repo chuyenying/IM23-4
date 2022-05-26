ebd4582ea5e611aef6911a73e22bbd034c6fa9ff
chuyenying <410835046@gms.ndhu.edu.tw>
2022-05-26 16:05:02 +0800

    0526 16:03
    - 把對話每個人的角度拉好
    - 更改了同學A的collider範圍，調整更大讓對話更好觸發
    - 停用程式碼修正，新增新的程式碼

--- Unity/First version/Assets/章節/第一章/Scripts/停或啟用腳本/WhenConversaction.cs
+++ Unity/First version/Assets/章節/第一章/Scripts/停或啟用腳本/WhenConversaction.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class WhenConversaction : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject robot_anim;
     [SerializeField] private GameObject walk_run_music;
 
     public void close()
     {
         player.GetComponent<controlpeople>().enabled = false;
+        robot_anim.GetComponent<ChaMove>().enabled = false;
+        robot_anim.GetComponent<Animator>().enabled = false;
         walk_run_music.SetActive(false);
     }    
     public void open()
     {
         player.GetComponent<controlpeople>().enabled = true;
+        robot_anim.GetComponent<ChaMove>().enabled = true;
+        robot_anim.GetComponent<Animator>().enabled = true;
+
         walk_run_music.SetActive(true);
     }
 }
