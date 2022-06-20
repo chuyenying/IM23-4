64a337e210733f9b5bfded4b423144cf00e3802d
chuyenying <410835046@gms.ndhu.edu.tw>
2022-06-20 17:40:54 +0800

    2022/6/20
    - 加入粒子效果
    - [第二章] 劇情加分

--- Unity/First version/Assets/章節/第二章/Scripts/SelectHalf_OpenDoor.cs
+++ Unity/First version/Assets/章節/第二章/Scripts/SelectHalf_OpenDoor.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class SelectHalf_OpenDoor : MonoBehaviour
 {
+
     private int count;
     [SerializeField] private Animator selectHalf_door;
     [SerializeField] private AudioSource selectHalf_door_music;
     private bool play = false;
     private int choose_half = 3;
+    
     void Update()
     {
+      
         if (count == choose_half && !play)
-        {
+        { 
             selectHalf_door_music.Play();
             selectHalf_door.SetTrigger("SelectOb_Half");
             play = !play;
+
         }
     }
-    
-    void FixedUpdate()
-    {
-        count = GameObject.Find("Selection_ob").GetComponent<Selection_ob>().ob_count;
-        Debug.Log(count);
-    }
 }
+
