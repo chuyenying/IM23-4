4d613bf09d9933c667152ed7045d585921a55cbe
chuyenying <410835046@gms.ndhu.edu.tw>
2022-07-24 19:13:35 +0800

    07/24 19:12
    - [第三章] 選物件
    
    - 電腦加入小遊戲(完善)
    
    - [第二章] 腳色修正-妹妹不適用目前有的轉身動畫，如果套上去後妹妹會掉下去。需要有人重新製作動畫。

--- Unity/First version/Assets/章節/第二章/Scripts/OpenComputer.cs
+++ Unity/First version/Assets/章節/第二章/Scripts/OpenComputer.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class OpenComputer : MonoBehaviour
 {
-    [SerializeField] private GameObject camera_computer, camera_peo , walk_run_music;
-    private bool open = false , trigger=false;
+    [SerializeField] private GameObject camera_computer, camera_peo, walk_run_music;
+    private bool open = false, trigger = false, ClickGameButton = false;
     [SerializeField] private GameObject E_image, Text_OpenComputer, Text_CloseComputer;
     private void Awake()
     {
@@ -16,12 +16,14 @@ public class OpenComputer : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && trigger)
+        Debug.Log($"Open:{open} , ClickGameButton:{ClickGameButton}");
+        if (Input.GetKeyDown(KeyCode.E) && trigger && !ClickGameButton)
         {
             open = !open;
             camera_computer.SetActive(open);
             camera_peo.SetActive(!open);
         }
+
     }
     private void OnTriggerStay(Collider other)
     {
@@ -29,7 +31,7 @@ public class OpenComputer : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             trigger = true;
-            if (!open)  //還沒打開電腦
+            if (!open && !ClickGameButton)  //還沒打開電腦也沒開遊戲
             {
                 E_image.SetActive(true);
                 Text_OpenComputer.SetActive(true);
@@ -38,7 +40,7 @@ public class OpenComputer : MonoBehaviour
                 GameObject.Find("主角").GetComponent<controlpeople>().enabled = true;
                 walk_run_music.SetActive(true);
             }
-            else if (open)  //已經打開電腦
+            else if (open && !ClickGameButton)  //已經打開電腦且沒有打開遊戲
             {
                 E_image.SetActive(true);
                 Text_OpenComputer.SetActive(false);
@@ -57,5 +59,21 @@ public class OpenComputer : MonoBehaviour
         Text_OpenComputer.SetActive(false);
         Text_CloseComputer.SetActive(false);
         trigger = false;
+    }
+
+    public void ClickGame() //有可能是打開遊戲或是關掉遊戲
+    {
+        ClickGameButton = !ClickGameButton;
+        ClickGameButtonAndCannotShow_EImage();
+    }
+
+    private void ClickGameButtonAndCannotShow_EImage()  //如果是打開遊戲的時候就不顯示UI
+    {
+        if (ClickGameButton)
+        {
+            E_image.SetActive(false);
+            Text_OpenComputer.SetActive(false);
+            Text_CloseComputer.SetActive(false);
+        }
     }
 }
