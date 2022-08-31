59e71724234c9130957530e436798e7979898770
ritakao0424 <kao900424@gmail.com>
2022-08-31 21:18:02 +0800

    0831
    第三章換場+對話修正(原本的第三章手機介面被刪掉了要再重弄)

---
+++ Unity/First version/Assets/章節/第三章/SCRIPT/ChangeSceneEP3.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeSceneEP3 : MonoBehaviour
+{
+public void DialogueToChangeScene()
+    {
+        SceneManager.LoadScene("®a");
+    }
+}
