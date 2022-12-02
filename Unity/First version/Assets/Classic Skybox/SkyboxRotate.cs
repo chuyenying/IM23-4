16d3a5bdd7583ef0bcb99030a288c0fc520493be
ritakao0424 <kao900424@gmail.com>
2022-12-03 01:23:48 +0800

    1203
    結局二主角拿衣服

--- Unity/First version/Assets/Classic Skybox/SkyboxRotate.cs
+++ Unity/First version/Assets/Classic Skybox/SkyboxRotate.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkyboxRotate : MonoBehaviour
-{
+//public class SkyboxRotate : MonoBehaviour
+//{
     // Start is called before the first frame update
-    public float RotateSpeed = 1.2f;  //天空旋轉的速度
+ //   public float RotateSpeed = 1.2f;  //天空旋轉的速度
       
     // Update is called once per frame
-    void Update()
-    {
-        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);  //隨時間更新旋轉的角度
-    }
-}
+  //  void Update()
+ //   {
+ //       RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);  //隨時間更新旋轉的角度
+ //   }
+//}
