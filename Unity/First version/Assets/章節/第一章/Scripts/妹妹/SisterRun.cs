a754e68681621170b49de41f66cf9c9169d06b27
Opal0811 <410835022@gms.ndhu.edu.tw>
2022-12-04 19:52:40 +0800

    1204 萍萍
    上傳手機UI、dialog system、庭恩做到一半

--- Unity/First version/Assets/章節/第一章/Scripts/妹妹/SisterRun.cs
+++ Unity/First version/Assets/章節/第一章/Scripts/妹妹/SisterRun.cs
@@ -3,45 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Playables;
-using UnityEngine.AI;
-
-
 public class SisterRun : MonoBehaviour
 {
-    [SerializeField] private GameObject sis;
-    [SerializeField] private GameObject sis_ChangeScenePos;
-    [SerializeField] private GameObject sisdes;
-    Vector3 Pos;
-    [SerializeField] private GameObject player;
-    public GameObject cam1, cam2;
-    [SerializeField] private NavMeshAgent sis_agent;
+    public GameObject sis;
+    public GameObject player;
+    bool over = false;
 
-    //public PlayableDirector SisTimeline;
-    [SerializeField] private Animator sisanim;
-
+    public PlayableDirector SisTimeline;
     public void sisRunToChangeScene()
     {
-        sis_ChangeScenePos.SetActive(true);
-        sisdes.GetComponent<NPCdes>().enabled = false;
-        sis.GetComponent<NPC>().enabled = false;
-        
-        cam1.SetActive(false);
-        cam2.SetActive(true);
 
         player.gameObject.transform.SetParent(sis.transform);
-
-        Pos = new Vector3(-6.51534128f, 2.7019999f, 10.0555086f);
-        //sisdes.transform.position = Pos;
-
-        sis_agent.speed = 1;
-        sis_agent.SetDestination(Pos);
-
-        sisanim.ResetTrigger("Walk");
-        sisanim.ResetTrigger("Idle");
-        sisanim.SetTrigger("holdhand");
-        
+        SisTimeline.Play();
         player.GetComponent<controlpeople>().enabled = false;
-        player.gameObject.transform.localPosition = new Vector3(-1f, 0f, 0f);
+        player.gameObject.transform.localPosition= new Vector3(-1f, 0f, 0f);
+        StartCoroutine(wait());
+    }
 
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(11f);
+        SceneManager.LoadScene("®a");
     }
 }
---
+++ Unity/First version/Assets/章節/結局二/Script/Letter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Letter : MonoBehaviour
+{
+    Animator animator;
+    public float speed = 2;
+    bool walk = false;
+
+    void Start()
+    {
+        Invoke("letter", 10);
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (walk == true)
+        {
+            Vector3 des = new Vector3(-1.30999994f, 2.65249157f, 26.8369999f);
+            this.transform.Translate(Vector3.forward * Time.deltaTime * speed); ;
+            if (this.transform.position == des)
+            {
+                transform.Rotate(0, 90, 0);
+                animator.SetBool("Give letter", true);
+                animator.SetBool("Give letter", false);
+                animator.SetBool("walk", false);
+            }
+        }
+    }
+    void letter()
+    {
+        animator.SetBool("walk", true);
+        walk = true;
+    }
+}
