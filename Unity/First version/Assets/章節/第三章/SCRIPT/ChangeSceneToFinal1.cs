a14e7b818254045f6b29da4eb8e484b8c7d90646
chuyenying <410835046@gms.ndhu.edu.tw>
2022-12-07 12:13:53 +0800

    12/7 12:13
    第三章咖啡廳串接完成。

---
+++ Unity/First version/Assets/章節/第三章/SCRIPT/ChangeSceneToFinal1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeSceneToFinal1 : MonoBehaviour
+{
+    public void ChangeSceneFromEP3ToFinal1House()
+    {
+        SceneManager.LoadScene("結局一-家");
+    }
+}
